22b0af406c832e301ebe57a7ef9a9a50838cec36
olgun-yilmaz <olgunyilm@gmail.com>
2025-01-19 20:42:24 +0300

    Added data visualizations

---
+++ app/src/main/java/com/olgunyilmaz/cryptocurrencyproject/adapter/RecyclerViewAdapter.java
@@ -0,0 +1,67 @@
+package com.olgunyilmaz.cryptocurrencyproject.adapter;
+
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.olgunyilmaz.cryptocurrencyproject.R;
+import com.olgunyilmaz.cryptocurrencyproject.model.CryptoModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.RowHolder> {
+    private ArrayList<CryptoModel> cryptoList;
+    private String [] colors = {"#2c3e50","#C39BD3","#d5d8dc","#e67e22","#82e0aa","#145a32","#ebdef0"};
+
+    public RecyclerViewAdapter(ArrayList<CryptoModel> cryptoList) {
+        this.cryptoList = cryptoList;
+    }
+
+
+
+    @NonNull
+    @Override
+    public RowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.row_layout,parent,false);
+        return new RowHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RowHolder holder, int position) {
+        holder.bind(cryptoList.get(position),colors,position);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return cryptoList.size();
+    }
+
+    public class RowHolder extends RecyclerView.ViewHolder {
+        TextView textName;
+        TextView textPrice;
+
+        public RowHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        public void bind(CryptoModel cryptoModel, String[] colors, Integer position){
+            itemView.setBackgroundColor(Color.parseColor(colors[position % colors.length]));
+            textName = itemView.findViewById(R.id.textName);
+            textPrice = itemView.findViewById(R.id.textPrice);
+            textName.setText(cryptoModel.currency);
+            textPrice.setText(cryptoModel.price);
+            System.out.println("end of  the bind");
+
+        }
+
+    }
+
+}
--- app/src/main/java/com/olgunyilmaz/cryptocurrencyproject/view/MainActivity.java
+++ app/src/main/java/com/olgunyilmaz/cryptocurrencyproject/view/MainActivity.java
@@ -1,85 +1,102 @@
 package com.olgunyilmaz.cryptocurrencyproject.view;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.olgunyilmaz.cryptocurrencyproject.R;
+import com.olgunyilmaz.cryptocurrencyproject.adapter.RecyclerViewAdapter;
 import com.olgunyilmaz.cryptocurrencyproject.model.CryptoModel;
 import com.olgunyilmaz.cryptocurrencyproject.service.CryptoAPI;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
+import io.reactivex.disposables.CompositeDisposable;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-    ArrayList <CryptoModel> cryptoModels;
-    private final  String BASE_URL = "https://raw.githubusercontent.com/";
+
+    ArrayList<CryptoModel> cryptoModels;
+    private String BASE_URL = "https://raw.githubusercontent.com/";
     Retrofit retrofit;
     RecyclerView recyclerView;
+    RecyclerViewAdapter recyclerViewAdapter;
+
+    CompositeDisposable compositeDisposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         recyclerView = findViewById(R.id.recyclerView);
 
-        // Retrofit & JSON
+        //Retrofit & JSON
+
         Gson gson = new GsonBuilder().setLenient().create();
 
         retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .build();
 
-        getDataFromAPI();
 
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        loadData();
+
     }
 
-    private void getDataFromAPI(){
-        CryptoAPI cryptoAPI = retrofit.create(CryptoAPI.class);
-        System.out.println("class");
-        Call <List<CryptoModel>> call =  cryptoAPI.getData();
+    private void loadData(){
 
+        final CryptoAPI cryptoAPI = retrofit.create(CryptoAPI.class);
+
+
+        Call<List<CryptoModel>> call = cryptoAPI.getData();
 
         call.enqueue(new Callback<List<CryptoModel>>() {
             @Override
             public void onResponse(Call<List<CryptoModel>> call, Response<List<CryptoModel>> response) {
-                if (response.isSuccessful()){
-                    List <CryptoModel> responseList = response.body();
+                if (response.isSuccessful()) {
+                    List<CryptoModel> responseList = response.body();
                     cryptoModels = new ArrayList<>(responseList);
 
-                    for (CryptoModel cryptoModel : cryptoModels){
-                        System.out.println(cryptoModel.currency);
-                    }
+                    //RecyclerView
+                    recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+                    recyclerViewAdapter = new RecyclerViewAdapter(cryptoModels);
+                    recyclerView.setAdapter(recyclerViewAdapter);
+
+
+
                 }
             }
 
             @Override
             public void onFailure(Call<List<CryptoModel>> call, Throwable t) {
                 t.printStackTrace();
-
             }
         });
 
+
+
+    }
+    
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        compositeDisposable.clear();
     }
 }
